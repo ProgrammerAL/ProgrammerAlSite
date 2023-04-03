6286cca6e4e5dc5b12feafa24fd4cc469095fa00
Al Rodriguez <ProgrammerAl@outlook.com>
2023-04-02 21:07:00 -0400

    Started some work to re-do the UI

---
+++ src/ProgrammerAl.Site/ProgrammerAl.Site/Components/HeaderNavBarComponent.razor.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.AspNetCore.Components;
+
+namespace ProgrammerAl.Site.Components;
+
+public partial class HeaderNavBarComponent : ComponentBase
+{
+}
