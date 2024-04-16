ef55f579445d65c167ec02bd28990c270a34fc40
Al Rodriguez <ProgrammerAl@outlook.com>
2024-04-16 14:08:14 -0400

    Removed the extra copy of files to the same folder we're uploading to

--- src/ProgrammerAl.Site/DynamicContentUpdater/Outputters/PostStaticHtmlOutputter.cs
+++ src/ProgrammerAl.Site/DynamicContentUpdater/Outputters/PostStaticHtmlOutputter.cs
@@ -30,19 +30,20 @@ public class PostStaticHtmlOutputter
         //Create static html files for each blog post entry
         foreach (var post in allPosts)
         {
-            //Output all other files too, for example image assets for a blog post
-            //  This includes the comic image if the post has one
-            foreach (var postFile in Directory.EnumerateFiles(post.PostDirectoryLocalPath))
-            {
-                string fileName = Path.GetFileName(postFile);
-                string outputFilePathForFile = $"{outputfolderPath}/{post.TitleLink}/{fileName}";
-                File.Copy(postFile, outputFilePathForFile, overwrite: true);
-            }
-
             var staticHtml = await engine.CompileRenderAsync<PostEntry>("Post.cshtml", post);
 
             string outputFilePath = $"{outputfolderPath}/{post.TitleLink}/post.html";
             File.WriteAllText(outputFilePath, staticHtml);
+
+            //Not needed. This copies files to their existing location
+            ////Output all other files too, for example image assets for a blog post
+            ////  This includes the comic image if the post has one
+            //foreach (var postFile in Directory.EnumerateFiles(post.PostDirectoryLocalPath))
+            //{
+            //    string fileName = Path.GetFileName(postFile);
+            //    string outputFilePathForFile = $"{outputfolderPath}/{post.TitleLink}/{fileName}";
+            //    File.Copy(postFile, outputFilePathForFile, overwrite: true);
+            //}
         }
 
         Console.WriteLine($"Completed outputting static html files...");
